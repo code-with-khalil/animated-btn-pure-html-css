6b91d43f7dd522f38a1c94bda501347573d48117
Muhammad Khalil <m.khalilbissnus00@gmail.com>
2024-01-30 09:29:37 +0500

    Update first.js

--- first.js
+++ first.js
@@ -1,18 +1,4 @@
-// const profile = {
-//     fullName : "shradha khapra",
-//     isFollow : true,
-//     posts : 195,
-//     follwers : "569k",
-//     following : 4,
-//     handel : "shradha khapra",
-//     bio : "apna college, microsoft ex, "
-// }
-/*let num = prompt("Enter a Number!")
-if(num % 7 === 0){
-    console.log(num,"is a multiple of 7")
-}else{
-    console.log(num,"is NOT a multiple of 7")
-}*/function myfunc(a) {
+ function myfunc(a) {
     let marks = prompt("enter marks to check your grade!")
     if(marks >= 101){
         alert("ONLY-1 - 100 -NUMERS")
